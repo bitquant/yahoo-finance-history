15ff539b682dc17b226dcf4a7bb59da9fab0927b
bitquant <bitquant@bitquant.com>
2020-04-15 21:30:05 -0500

    replaced request with fetch

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 'use strict';
+var fetch = require('fetchout');
+var setCookie = require('set-cookie-parser');
 
-const request = require('request-promise-native').defaults({timeout: 9999});
+fetch.defaultTimeout(10 * 1000);
 
 var getPriceHistory = async (symbol) => {
 
     // Convert symbols like BRK.B to BRK-B to make yahoo happy
     symbol = symbol.replace(/\./g, "-");
 
-    var options = {
-        uri: "https://finance.yahoo.com/quote/" + symbol + "/history?p=" + symbol,
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:45.0) Gecko/20100101 Firefox/45.0'
-        },
-        jar: request.jar()
-    };
+    var options = { headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:75.0) Gecko/20100101 Firefox/75.0'
+    }};
+
     // First get the cookie and crumb we need
-    var responseBody = await request(options);
+    var response = await fetch(`https://finance.yahoo.com/quote/${symbol}/history?p=${symbol}`, options);
+    if (response.status !== 200) {
+        throw new Error(`failed to fetch yahoo finance: ${response.status}`);
+    }
+    var cookieHeader = response.headers.get('set-cookie');
+    var cookiesSet = setCookie.parse(setCookie.splitCookiesString(cookieHeader));
+    options.headers['Cookie'] = getRequestCookie(cookiesSet);
 
     // Search for the crumb in the response data
+    var responseBody = await response.text();
     var crumbMatch = responseBody.match(/("CrumbStore":{[^}]+})/);
-    var crumbObject = JSON.parse("{" + crumbMatch + "}");
+    var crumbObject = JSON.parse("{" + crumbMatch[1] + "}");
 
     // Setup query timestamps
     let startEpoch = -2147483648;
@@ -27,21 +33,40 @@ var getPriceHistory = async (symbol) => {
 
     // Grab the history data
     let baseUrl = "https://query1.finance.yahoo.com/v7/finance/download/"
-    var args = `${symbol}?period1=${startEpoch}&period2=${endEpoch}&interval=1d&events=history&crumb=${crumbObject.CrumbStore.crumb}`
-    options.uri = baseUrl + args;
-    var priceHistory = await request(options);
+    var args = `${symbol}?period1=${startEpoch}&period2=${endEpoch}&interval=1d&events=history&crumb=${crumbObject.CrumbStore.crumb}`;
+    var priceHistoryResponse = await fetch(baseUrl + args, options);
+    if (priceHistoryResponse.status !== 200) {
+        throw new Error(`failed to fetch price history: ${priceHistoryResponse.status}`)
+    }
+    var priceHistory = await priceHistoryResponse.text();
 
     // Grab the dividend data
     args = `${symbol}?period1=${startEpoch}&period2=${endEpoch}&interval=1d&events=div&crumb=${crumbObject.CrumbStore.crumb}`
-    options.uri = baseUrl + args;
-    var dividendHistory = await request(options);
+    var dividendHistoryResponse = await fetch(baseUrl + args, options);
+    if (dividendHistoryResponse.status !== 200) {
+        throw new Error(`failed to fetch dividend history: ${dividendHistoryResponse.status}`)
+    }
+    var dividendHistory = await await dividendHistoryResponse.text();
 
     // Grab the split data
     args = `${symbol}?period1=${startEpoch}&period2=${endEpoch}&interval=1d&events=split&crumb=${crumbObject.CrumbStore.crumb}`
-    options.uri = baseUrl + args;
-    var splitHistory = await request(options);
+    var splitHistoryResponse = await fetch(baseUrl + args, options);
+    if (splitHistoryResponse.status !== 200) {
+        throw new Error(`failed to fetch split history: ${splitHistoryResponse.status}`)
+    }
+    var splitHistory = await await splitHistoryResponse.text();
 
     return { priceHistory, dividendHistory, splitHistory };
 };
+
+function getRequestCookie(cookies) {
+    return cookies.reduce((result, current) => {
+        if (result.length > 0) {
+            result += '; ';
+        }
+        result += `${current.name}=${current.value}`
+        return result;
+    }, '');
+}
 
 exports.getPriceHistory = getPriceHistory;
